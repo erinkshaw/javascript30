4f98baffb88a435326868ff7b9989d48b50c9764
Erin Shaw <erin.kimberly.shaw@gmail.com>
2018-03-07 16:50:03 -0500

    finished i guess

--- projects/pokeroll/index.js
+++ projects/pokeroll/index.js
@@ -1,15 +1,43 @@
 
-const images = document.querySelectorAll('img')
+const pokemon = document.querySelectorAll('.pokemon')
+const pokeball = document.querySelector('#center')
+const input = document.querySelector('input')
+const deg = 'deg'
+let speed = 500
+let interval = setInterval(rotatePokemon, speed)
 
-function growImg(img) {
-  console.log(img)
+function updateSpeed() {
+  speed = this.value
+  clearInterval(interval)
+  interval = setInterval(rotatePokemon, speed)
 }
-images.forEach(img => {
-  img.addEventListener('mouseover', growImg)
-})
-console.log(images)
 
+function pokeSound(event) {
+  if (event.keyCode === 32) {
+    const audio = document.querySelector('#audio')
+    audio.currentTime = 0;
+    audio.play()
+    pokeball.classList.add('playing')
+  }
+}
 
+function pokeDown(event) {
+  if (event.propertyName !== 'transform') return;
+  event.target.classList.remove('playing');
+}
+
+function rotatePokemon() {
+  pokemon.forEach(poke => {
+    let currentDeg = Number(getComputedStyle(document.body).getPropertyValue(`--${poke.id}`).slice(0, -3))
+    currentDeg += 10
+    document.documentElement.style.setProperty(`--${poke.id}`, currentDeg + deg)
+  })
+}
+
+input.addEventListener('change', updateSpeed)
+input.addEventListener('mousemove', updateSpeed)
+window.addEventListener('keydown', pokeSound)
+window.addEventListener('transitionend', pokeDown)
 // set up poke circle with pokeball in the middle
 
 // have it rotate
